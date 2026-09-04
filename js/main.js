20e1548e3151fb4ee13a4aa48e9a991b04cfbf13
Alexis Pineiro <alexispineiro126@gmail.com>
2026-09-04 09:41:55 -0300

    mi primer commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let precio = parseInt(prompt("Ingrese el precio del producto:"))
+
+let cantidad = parseInt(prompt("Ingrese la cantidad del producto:"))
+
+let descuento = parseInt(prompt("Ingrese el porcentaje de descuento:"))
+
+function calculartotal (precio, cantidad){
+    return precio * cantidad
+}
+
+const aplicardescuento = function(total, descuento) {
+    return total - (total * descuento / 100)
+}
+
+const total = (total) =>{
+    alert("El total a pagar es: $" + total)
+}
+
+let totalSinDescuento = calculartotal(precio, cantidad)
+let totalConDescuento = aplicardescuento(totalSinDescuento, descuento)
+total(totalConDescuento)
