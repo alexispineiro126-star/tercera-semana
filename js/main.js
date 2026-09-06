30a32a9f7db912585c211cd73b1e57abe593014f
Alexis Pineiro <alexispineiro126@gmail.com>
2026-09-06 09:26:12 -0300

    actualice repositorio

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
-let precio = parseInt(prompt("Ingrese el precio del producto:"))
+let consulta = "si";
 
-let cantidad = parseInt(prompt("Ingrese la cantidad del producto:"))
+while (consulta === "si") {
 
-let descuento = parseInt(prompt("Ingrese el porcentaje de descuento:"))
+let precio = parseInt(prompt("Ingrese el precio del producto:"));
+let cantidad = parseInt(prompt("Ingrese la cantidad del producto:"));
+let descuento = parseInt(prompt("Ingrese el porcentaje de descuento:"));
 
-function calculartotal (precio, cantidad){
-    return precio * cantidad
+function calculartotal(precio, cantidad) {
+return precio * cantidad;
 }
 
 const aplicardescuento = function(total, descuento) {
-    return total - (total * descuento / 100)
+return total - (total * descuento / 100);
 }
 
-const total = (total) =>{
-    alert("El total a pagar es: $" + total)
+const total = (total) => {
+alert("El total a pagar es: $" + total);
 }
 
-let totalSinDescuento = calculartotal(precio, cantidad)
-let totalConDescuento = aplicardescuento(totalSinDescuento, descuento)
-total(totalConDescuento)
+let totalSinDescuento = calculartotal(precio, cantidad);
+let totalConDescuento = aplicardescuento(totalSinDescuento, descuento);
+total(totalConDescuento);
+
+consulta = prompt("¿Desea realizar otra compra? (si/no)").toLowerCase();
+}
