e26b3f4feee17ca87677c4b4e6bb28dddea5e936
Alexis Pineiro <alexispineiro126@gmail.com>
2026-09-16 19:51:52 -0300

    actualice repositorio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const panaderia = []
 
 function agregarproducto() {
-    let producto = (prompt("Agregue el nombre del productos que desea comprar: \n1.Pan \n2.Galletitas \n3.Facturas \n4.Chipitas \n5Torta "))
+    let producto = (prompt("Agregue el nombre del productos que desea comprar: \n1.Pan \n2.Galletitas \n3.Facturas \n4.Chipitas \n5.Torta "))
     panaderia.push(producto)
 }
 function agregarproducto2() {
-    let producto = (prompt("Agregue otro producto: \n1.Pan \n2.Galletitas \n3.Facturas \n4.Chipitas \n5Torta"))
+    let producto = (prompt("Agregue otro producto: \n1.Pan \n2.Galletitas \n3.Facturas \n4.Chipitas \n5.Torta"))
     panaderia.unshift(producto)
 }
 
@@ -17,23 +17,32 @@ const mostrarproductos = function(){
     }
 }
 function eliminarproductos(){
-    let eliminar = prompt("Eliminar algún producto:")
+    let eliminar = prompt("Elimina algún producto:")
     if (panaderia.includes(eliminar)){
         let  posicion =  panaderia.indexOf(eliminar)
         panaderia.splice(posicion,1)
-    alert("Producto eliminado")
+    alert("eliminaste el producto: " + eliminar)
     }else{
         alert("Ese prodcuto no existe")
     }
 }
+function eliminar2() {
+    let confirmar = prompt("Desea eliminar el último producto? (si/no)").toLowerCase()
+    if (confirmar === "si") {
+        let eliminado = panaderia.pop()
+        alert("Se ha eliminado el producto: " + eliminado)
+    }
+}
+
 let consulta = "si"
 while (consulta === "si") {
 agregarproducto()
 agregarproducto2()
 mostrarproductos()
 eliminarproductos()
+eliminar2()
 
-consulta = prompt("¿Desea agregar otro producto? (si/no)").toLowerCase();
+consulta = prompt("Desea agregar otro producto? (si/no)").toLowerCase();
 }
 
 
